81e3056734b9bb9616841940ccf758495767e117
Ocean <oceanalbin@live.nl>
2019-09-23 10:31:45 +0200

    adds

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ var port = 3000;
 var app = express();
 
 // STEP 1: Setting up the boilerplate and routing
-app.get('/', function(req, res){
+app.get('/', function(req, res)
 
+// Storing url
+
+  var url = 'https://nl.wikipedia.org/wiki/Fyllotaxis'
+
+// Making HTTP request
+  request(url, options: function(error, response, html))
+    if(!error) {
+        res.send(html)
+    }
     //All the web scraping magic will happen here
-    res.send('Hello World')
+    // res.send('Hello World')
 });
 
 app.listen(port);
