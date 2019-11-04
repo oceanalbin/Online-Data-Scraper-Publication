d2ea1e2fad79066176d02ff28a8f7ae6c9057c61
Ocean <oceanalbin@live.nl>
2019-11-04 12:08:54 +0100

    .

--- wiki-output-list.js
+++ wiki-output-list.js
@@ -1,4 +1,4 @@
-var wiki_output-list = {
+var wiki_output_list = {
     "articleTitle": "Lists of earthquakes",
     "articleImg": "//upload.wikimedia.org/wikipedia/commons/thumb/2/29/Map_of_earthquakes_1900-.svg/400px-Map_of_earthquakes_1900-.svg.png",
     "articleParagraph": "\nThe following is a list of earthquake lists, and of top earthquakes by magnitude and fatalities.\n",
